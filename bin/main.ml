44ca5ca4a3c541aa5faa47d2574a28a0c0201b26
Madhav Kumar <47245900+kumar-ish@users.noreply.github.com>
2023-11-20 12:14:58 +0800

    add trie construction and matching functions

--- bin/main.ml
+++ bin/main.ml
@@ -1,9 +1,32 @@
 open! Core
 
-let () =
-  In_channel.read_lines "cedict_ts.truncated.u8"
-  |> List.map ~f:Chinese_dag.Parser.parse_chinese_line
-  |> List.iter ~f:(fun x ->
-         print_s
-           [%message (Result.ok_or_failwith x : Chinese_dag.Parser.Definition.t option)])
+let get_s (d : Chinese_dag.Parser.Definition.t) = d.simplified
+
+let global_trie =
+  In_channel.read_lines "cedict_ts.u8"
+  |> List.filter_map ~f:(fun x ->
+         Chinese_dag.Parser.parse_chinese_line x |> Result.ok_or_failwith)
+  |> List.map ~f:get_s
+  |> Chinese_dag.Trie.construct_trie
 ;;
+
+let () =
+  let matches =
+    Chinese_dag.Trie.find_longest_matching_symbols
+      global_trie
+      "1、打破关住自己的门\n\n\
+      \      一个木匠做得一手好门。他给自己家做了一扇门，他认为这门用料实在，做工精良，一定会经久耐用。 \
+       过了一段时间，门的钉子锈了，掉下一块板，木匠找出一颗钉子补上，门又完好如初。不久又掉了一颗钉子，木匠就换上一颗钉子。后来，又有一块板坏了，木匠就又找出一块板换上。再后来，门闩坏了，木匠又换了一个门闩„„ \
+       若干年后，这扇门虽经无数次破损，但经过木匠的精心修理，仍坚固耐用。木匠对此甚是自豪：多亏有了这门手艺，不然门坏了还不知如何是好。 \
+       忽然有一天，邻居对他说：“你是木匠，你看看你家这门！”木匠仔细一看，才发觉邻居家的门一扇扇样式新颖、质地优良，而自己家的门又老又破，满是补丁。木匠明白了，是自己的这种门手艺阻碍了自家“门”的发展。\n\
+      \      "
+  in
+  let table = Chinese_dag.Trie.gather_statistics matches in
+  Hashtbl.to_alist table
+  |> List.sort ~compare:(fun (_, a) (_, b) -> b - a)
+  |> List.iter ~f:(fun (_key, _data) -> printf "%s %d" _key _data)
+;;
+
+(* |> List.iter ~f:(fun x ->
+         print_s
+           [%message (Result.ok_or_failwith x : Chinese_dag.Parser.Definition.t option)]) *)
--- lib/trie.ml
+++ lib/trie.ml
@@ -1,10 +1,112 @@
-module TrieNode = struct
-  type t =
-    { character : Uchar.t
-    ; is_end : bool
-    ; children : (Uchar.t, t) Hashtbl.t
-    }
+open Core
+
+type t =
+  { character : Uchar.t
+  ; mutable is_end : bool
+  ; children : (Uchar.t, t) Hashtbl.t
+  }
+
+let create_node character =
+  let children = Hashtbl.create (module Uchar) in
+  let is_end = false in
+  { character; is_end; children }
+;;
+
+let construct_trie _words =
+  let root_node = create_node (Uchar.of_char ' ') in
+  let uchar_folder current_node _ = function
+    | `Uchar u ->
+      (match Hashtbl.find current_node.children u with
+      | Some a -> a
+      | None ->
+        let new_node = create_node u in
+        let (_ : [ `Duplicate | `Ok ]) =
+          Hashtbl.add current_node.children ~key:u ~data:new_node
+        in
+        new_node)
+    | `Malformed _ -> failwith "malformed input"
+  in
+  List.iter _words ~f:(fun s ->
+      let bottom_node = Uutf.String.fold_utf_8 uchar_folder root_node s in
+      bottom_node.is_end <- true);
+  root_node
+;;
+
+let find_in_trie trie word =
+  let uchar_folder a _ char =
+    match a with
+    | None -> None
+    | Some a ->
+      (match char with
+      | `Uchar u ->
+        (match Hashtbl.find a.children u with
+        | Some child -> Some child
+        | None -> None)
+      | `Malformed _ -> None)
+  in
+  match Uutf.String.fold_utf_8 uchar_folder (Some trie) word with
+  | Some _ -> true
+  | None -> false
+;;
+
+let create_string word =
+  let b = Buffer.create 4 in
+  List.iter word ~f:(Uutf.Buffer.add_utf_8 b);
+  Buffer.contents b
+;;
+
+(* Greedily finds longest-matching Chinese words from trie dictionary *)
+let find_longest_matching_symbols root_node text =
+  let uchar_folder ls _ = function
+    | `Uchar u -> u :: ls
+    | `Malformed _ -> ls
+  in
+  let uchar_list = List.rev @@ Uutf.String.fold_utf_8 uchar_folder [] text in
+  let rec find_longest_matching_word node (longest_match, acc) = function
+    | [] -> longest_match, acc
+    | uchar :: rest ->
+      (match Hashtbl.find node.children uchar with
+      | None -> longest_match, acc
+      | Some child ->
+        let longest_match, acc' =
+          let string_so_far = acc @ [ uchar ] in
+          if child.is_end
+          then Some string_so_far, string_so_far
+          else longest_match, string_so_far
+        in
+        find_longest_matching_word child (longest_match, acc') rest)
+  in
+  let rec find_matching_symbols = function
+    | [] -> []
+    | _ as uchar_list ->
+      let longest_match, _acc =
+        find_longest_matching_word root_node (None, []) uchar_list
+      in
+      (match longest_match with
+      | None -> find_matching_symbols (List.tl_exn uchar_list)
+      | Some match_ ->
+        match_ :: find_matching_symbols (List.drop uchar_list (List.length match_)))
+  in
+  find_matching_symbols uchar_list |> List.map ~f:create_string
+;;
+
+module Statistics = struct
+  type t = (string, int) Hashtbl.t
+
+  let create _ = Hashtbl.create (module String)
+
+  let update_count t key =
+    Hashtbl.update t key ~f:(function
+        | None -> 1
+        | Some count -> count + 1)
+  ;;
 end
+
+let gather_statistics words =
+  let table = Statistics.create () in
+  List.iter words ~f:(fun w -> Statistics.update_count table w);
+  table
+;;
 
 (*
             我T
@@ -18,5 +120,3 @@ end
 (* chinese_word *)
 (* pronunciation *)
 (* meaning *)
-
-(* Aho–Corasick algorithm *)
